246e4937bf867bcc85d4bc8b6026381f75f34197
unknown <tree.joanne@gmail.com>
2021-06-30 23:49:24 +0100

    components added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import trigger from './components/trigger';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <trigger/>
+<div>
+ <div>
+  <p>This page should give you a count of the number of days from today to Christmas Day, New Years Day and Valentines Day. </p> 
+  </div>
+
+  <div>
+    <ul id="days-until"></ul>
+  </div>
+
+  <div>
+    <button id="trigger">Show Days Until</button>
+  </div>
+</div>
+
+
     </div>
   );
 }
---
+++ src/components/trigger.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function trigger() {
+
+const today = new Date();
+const xmas = new Date(2021, 12, 25);
+const newYear = new Date(2022, 01, 01);
+const valentines = new Date(2022, 02, 14);
+const calculation = 24 * 3600 * 1000;
+
+function calculateDaysTo(date) {
+  const daysTo = parseInt((date - today) / calculation);
+  return daysTo;
+}
+
+const daysToXmas = calculateDaysTo(xmas);
+const daysToNewYear = calculateDaysTo(newYear);
+const daysToValentines = calculateDaysTo(valentines);
+
+$("#trigger").click(function () {
+  $("#days-until").append(
+    "<li>" +
+      daysToXmas.toString() +
+      " days until Christmas</li>" +
+      "<li>" +
+      daysToNewYear.toString() +
+      " days until New Year</li>" +
+      "<li>" +
+      daysToValentines.toString() +
+      " days until Valentines Day</li>"
+  );
+});
+}
+
+export default trigger;
+
